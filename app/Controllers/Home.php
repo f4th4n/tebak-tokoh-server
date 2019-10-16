11203a77fe0136c201d9f609b8560ebd05517077
fathan <wilfathan@gmail.com>
2019-10-16 14:40:20 +0700

    Update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,11 @@
 <?php namespace App\Controllers;
-use App\Models\QuestionModel;
+use App\Models\QuizModel;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-    $question_model = new QuestionModel();
+    $question_model = new QuizModel();
     $rows = $question_model->getQuestions();
     var_dump($rows);
     return view('welcome_message');
